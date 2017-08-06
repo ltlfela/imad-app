1c493d9c74c9b622a418aa467cf03fc24a86bb3f
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-08-06 17:59:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,10 +88,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get("/:articleName", function(req, res) {
+app.get("/  :articleName", function(req, res) {
     // articleName == article-one
     // articles[aarticlesname] == {} content object for article one
-    var articleName = reg.params.articleName;
+    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
 
