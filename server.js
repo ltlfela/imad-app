b1b34edff6ac1100613f8ddc6bebc5959a380e9b
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-08-19 17:24:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,10 +108,25 @@ app.get('/test-db', function(req, res) {
      });
 });
 
-app.get("/:articleName", function(req, res) {
+app.get("/article/:articleName", function(req, res) {
     // articleName == article-one
     // articles[aarticlesname] == {} content object for article one
-    var articleName = req.params.articleName;
+    
+    pool.query("SELECT * FROM article WHERE title =" + req.params.articleName, function(err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length === 0) {
+                res.status(404).send('Article not found');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+            }
+        });
+    }); 
+    res.send(createTemplate(articleData));
+    
    res.send(createTemplate(articles[articleName]));
 });
 
