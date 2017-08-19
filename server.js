ed04611856d627770ae74a902df38758c44de130
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-08-19 17:36:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get("/article/:articleName", function(req, res) {
     // articleName == article-one
     // articles[aarticlesname] == {} content object for article one
     
-    pool.query("SELECT * FROM article WHERE title ='" + req.params.articleName + "'", function(err, result) {
+    pool.query("SELECT * FROM article WHERE title =$!", [req.parms.articleName], function(err, result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
