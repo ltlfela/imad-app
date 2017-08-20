f6dfc912470799c14820502ea7b734461618eb19
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-08-20 19:30:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 var pool = new Pool(config);
 
 app.get('/test-db', function(req, res) {
