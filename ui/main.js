7ddb187502c7700d90e7195649522642a8547d12
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-08-20 19:41:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //Counter code
 var button = document.getElementById('counter');
 var counter = 0;
-button.onclick = function() {
+button.onclick = function () {
   
   counter = counter + 1;
    var span = dcoument.getElementById('count');
