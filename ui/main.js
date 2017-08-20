d80d54672bdb48b492258b2dcc28333b7d249656
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-08-20 21:46:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,23 @@ var counter = 0;
 
 button.onclick = function () {
   
-  counter = counter + 1;
-   var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
+  // Create a request object to the counter endpoint
+  var request = new XMLHttpRequest();
+  
+  // Capture the response and store it in a variable
+  request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.Done) {
+          // Take some action
+          if (request.status === 200) {
+              var counter = request.responseText;
+              span.innerHTML = counter.toString();
+          }
+      }
+      // NOt done yet
+  };
+  
+  // Make the request
+  request.open('GET', 'http://ltlfela.imad.hasura-app.io/counter', true);
+  
 };
 console.log("main.js loaded");
