211abd3ae75d4dc4dfe2b098fbc74e05d4a431d2
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-08-25 17:42:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ function hash (input, salt) {
 app.get('/hash/:input', function(req, res) {
     var hashedString = hash(req.params.input, 'this-is-salt');
     res.send(hashedString);
-}
+});
 
 
 var counter = 0;
