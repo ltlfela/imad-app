da4355d559886e19ec6918bc164c999f5d67caff
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-08-25 16:24:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,3 +115,16 @@ app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
 
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
