1a5c9f54bd87d0063ba3c8c041ca797ef2bb00f1
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-08-25 18:07:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var bodyParser = require('body-parser');
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'ltlfela',
@@ -70,6 +70,23 @@ app.get('/hash/:input', function(req, res) {
     res.send(hashedString);
 });
 
+app.post('/create-user', function (req, res) {
+    
+   //load the JSON content in the req.body variable from app.use
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(pasword, salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
+         if (err) {
+            res.status(500).send(err.toString());
+        } else {
+           
+           res.send('User successfully created: '+ username);
+        }
+        });
+});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
