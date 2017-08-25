9e643aae6671e24d6738786ce17fb4c4fde08988
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-08-25 23:08:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -117,8 +117,8 @@ submit3.onclick = function () {
   };
   
   // Tah hian create Id a ngai ang
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
+   username = document.getElementById('username').value;
+   password = document.getElementById('password').value;
   
   console.log(username);
   console.log(password);
