4f8fbb8806e5fa7747c2989af8992c173ff02c62
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-08-27 19:56:01 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -6,13 +6,13 @@ function loadCommentForm () {
         <h5>Submit a comment</h5>
         <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
         <br/>
-        <input type="submit" id="comm_submit" value="Submit" />
+        <input type="submit" id="submit" value="Submit" />
         <br/>
         `;
     document.getElementById('comment_form').innerHTML = commentFormHtml;
     
     // Submit username/password to login
-    var submit = document.getElementById('comm_submit');
+    var submit = document.getElementById('submit');
     submit.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
