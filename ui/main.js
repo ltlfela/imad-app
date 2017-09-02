b610f652c1635025d6c1588bb19a51741b3e53a2
ltlfela <30766470+ltlfela@users.noreply.github.com>
2017-09-02 23:58:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,6 @@ var loginHTML = `
 <div> 
 <input type="text" id="username" placeholder="username" />
 <input type="password" id="password" placeholder="password" />
-<br/>
 <input type="submit" value="Login" id="login_btn" />
 <input type="submit" value="Register" id="register_btn" />
 </div>
